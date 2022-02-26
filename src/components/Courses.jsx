b1b024611ba1ff5e2c2299494a76e50d2ec41bc7
Abdifatahz <faahid44@gmail.com>
2022-02-26 10:44:41 +0300

    Lesson 13 Adding Filters using useState.

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,15 +1,53 @@
+import { useState } from 'react'
 import Course from './Course'
 import courseData from '../courseData'
 
 export default function Courses(){
+    const [allCourses,setAllCourses] = useState(courseData);
+    const [filteredCourses,setFilteredCourses] = useState([]);
 
-    const courses = courseData.map(course => ( 
+    const filterData = ["All","Free","Paid","New"];
+
+    function getCourses(){
+        if(filteredCourses.length > 0){
+            return filteredCourses;
+        }else{
+            return allCourses
+        }
+    }
+
+    const courses = getCourses().map(course => ( 
         <Course key={course.id} title={course.title} price={course.price} isNew={course.isNew}/>
    ))
+   
+   function filetrByPrice(filterValue){
+        if(filterValue == "Free"){
+            return allCourses.filter(courses => courses.price == 0)
+        }else if(filterValue == "Paid"){
+            return allCourses.filter(courses => courses.price > 0)
+        }else{
+            return [];
+        }
+    }
+
+   function filterCourses(e){
+       const filterValue = e.target.innerHTML;
+       if(filterValue !== "All"){
+        setFilteredCourses(filetrByPrice(filterValue))
+       }else{
+        setFilteredCourses(allCourses);
+       }
+   }
 
     return (
-        <div className='courses'>
-            {courses}
-        </div>
+        <>  
+            <div className="filters">
+                {filterData.map(filter => <button onClick={filterCourses}>{filter}</button>)}
+            </div>
+            <div className='courses'>
+                {courses.length > 0 ? courses : "No Courses Found"}
+            </div>
+        </>
+       
     )
 }
