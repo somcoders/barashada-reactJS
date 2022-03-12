df8d08d06d217751d54ba946087aa3a7dccd31fa
somcoders <somalicoders@gmail.com>
2022-03-12 09:21:18 +0300

    Fix double equal warning.

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import Courses from './components/Courses'
 
 function App() {
     const [theme,setTheme] = useState("white");
-    const themeSwitcher = theme == "white" ? "black" : "white";
+    const themeSwitcher = theme === "white" ? "black" : "white";
 
     const changeTheme = () => {
         setTheme(themeSwitcher);
@@ -15,7 +15,7 @@ function App() {
             <span 
                 onClick={changeTheme}
                 style={{fontSize:"1.5em"}}
-                >{ theme == "white" ? "🌒" : "☀️"}
+                >{ theme === "white" ? "🌒" : "☀️"}
               
              </span>
             <Courses />
