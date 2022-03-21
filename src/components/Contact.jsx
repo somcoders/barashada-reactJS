8f2770d8d298ad1931e37c0a427c7f9a1857bea2
somcoders <somalicoders@gmail.com>
2022-03-21 14:42:35 +0300

    Lesson 24 Class based React 101.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{Component} from 'react';
+import { ThemeContext } from '../context/ThemeContextProvider';
 
-function Contact(props) {
-    return (
-        <div>
-            <h4>Contact us</h4>
-        </div>
-    );
+class Contact extends Component {
+
+    render(){
+        return (
+            <ThemeContext.Consumer>
+                {mode => (
+                    <div className={`container ${mode.theme}`}>
+                        <h4>Contact us </h4>
+                    </div>
+                )}
+            </ThemeContext.Consumer>
+        );
+    }
 }
 
 export default Contact;
